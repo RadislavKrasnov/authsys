9906bea39deee2ae4c0c8f296486e3c3f41dae5e
Radislav Krasnov <radislavkrasnoff@gmail.com>
2020-09-26 19:14:27 +0300

    add transition for burger menu

--- public/js/menu/mobile-menu.js
+++ public/js/menu/mobile-menu.js
@@ -2,10 +2,12 @@ function showBurgerMenu(burgerIcon) {
     burgerIcon.classList.toggle("change");
 
     var mobileLinks = document.getElementById("mobile-links");
-    if (mobileLinks.style.display === "block") {
-        mobileLinks.style.display = "none";
+    if (mobileLinks.style.visibility === "visible") {
+        mobileLinks.style.visibility = "hidden";
+        mobileLinks.classList.remove('mobile-menu-sidebar');
     } else {
-        mobileLinks.style.display = "block";
+        mobileLinks.style.visibility = "visible";
+        mobileLinks.classList.add('mobile-menu-sidebar');
     }
 }
 
@@ -18,11 +20,12 @@ window.onload = function() {
         if (element.target.parentElement.id !== 'mobile-burger-icon' &&
             element.target.id !== 'mobile-burger-icon' &&
             element.target.id !== 'mobile-links' &&
-            mobileLinks.style.display !== 'none') {
-            mobileLinks.style.display = 'none';
+            mobileLinks.style.visibility !== 'hidden') {
+            mobileLinks.style.visibility = 'hidden';
+            mobileLinks.classList.remove('mobile-menu-sidebar');
         }
 
-        if (mobileLinks.style.display === 'none' && burgerIcon.classList) {
+        if (mobileLinks.style.visibility === 'hidden' && burgerIcon.classList) {
             burgerIcon.classList.remove('change');
         }
     };
